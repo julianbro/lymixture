bff1c04ffed84c2353be6ea8d4966f4f4e469fee
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2024-02-06 14:03:16 +0100

    remove lyscripts clone

--- lyscripts/__main__.py
+++
@@ -1,9 +0,0 @@
-"""
-Utility for performing common tasks w.r.t. the inference and prediction tasks one
-can use the `lymph` package for.
-"""
-from lyscripts import main
-
-# I need another __main__ guard here, because otherwise pdoc tries to run this
-if __name__ == "__main__":
-    main()
